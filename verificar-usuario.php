31ab331022020d07d693c4091265dfb17b1ec5a6
Electriona <Electriona@users.noreply.github.com>
2023-09-27 09:14:19 -0300

    usucontra
    chell 666

--- verificar-usuario.php
+++ verificar-usuario.php
@@ -3,8 +3,8 @@
 // $usuario = $_POST["usuario"];
 // $contrasena = $_POST["contrasena"];
 //Luego se conectara a la base de datos
-$usuario = "ana";
-$contrasena = "123";
+$usuario = "chell";
+$contrasena = "666";
 
 if ($_POST["usuario"] == $usuario and $_POST["contraseña"] == $contrasena){
     echo "Accediste correctamente";
